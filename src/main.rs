c6a63a933b8676c606c191cfd8d0950ada24ec15
François Mockers <mockersf@gmail.com>
2019-03-15 01:38:09 +0100

    :sparkles: allow more config options

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,30 @@ use serde::{Deserialize, Serialize};
 
 use concourse_resource::*;
 
+mod message;
+use message::Message;
+
 struct Test {}
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Version {
     refid: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Source {
     url: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
 enum AlertType {
     Success,
     Failed,
     Started,
     Aborted,
-    Fixed,
-    Broke,
+    // Fixed,
+    // Broke,
     Custom,
 }
 
@@ -32,111 +35,24 @@ impl Default for AlertType {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
+#[serde(default)]
 struct OutParams {
     alert_type: AlertType,
     color: Option<String>,
-    concise: Option<bool>,
+    concise: bool,
     message: Option<String>,
-    channel: String,
+    channel: Option<String>,
 }
 
-#[derive(Serialize)]
-struct Message {
-    color: String,
-    message: String,
-    icon_url: String,
-}
-
-impl Message {
-    fn new(params: &OutParams) -> Message {
-        let mut message = match params.alert_type {
-            AlertType::Success => Message {
-                color: String::from("#32cd32"),
-                icon_url: String::from(
-                    "https://ci.concourse-ci.org/public/images/favicon-succeeded.png",
-                ),
-                message: String::from("Success"),
-            },
-            AlertType::Failed => Message {
-                color: String::from("#d00000"),
-                icon_url: String::from(
-                    "https://ci.concourse-ci.org/public/images/favicon-failed.png",
-                ),
-                message: String::from("Failed"),
-            },
-            AlertType::Started => Message {
-                color: String::from("#f7cd42"),
-                icon_url: String::from(
-                    "https://ci.concourse-ci.org/public/images/favicon-started.png",
-                ),
-                message: String::from("Started"),
-            },
-            AlertType::Aborted => Message {
-                color: String::from("#8d4b32"),
-                icon_url: String::from(
-                    "https://ci.concourse-ci.org/public/images/favicon-aborted.png",
-                ),
-                message: String::from("Aborted"),
-            },
-            AlertType::Fixed => Message {
-                color: String::from("#32cd32"),
-                icon_url: String::from(
-                    "https://ci.concourse-ci.org/public/images/favicon-succeeded.png",
-                ),
-                message: String::from("Fixed"),
-            },
-            AlertType::Broke => Message {
-                color: String::from("#d00000"),
-                icon_url: String::from(
-                    "https://ci.concourse-ci.org/public/images/favicon-failed.png",
-                ),
-                message: String::from("Broke"),
-            },
-            AlertType::Custom => Message {
-                color: String::from("#35495c"),
-                icon_url: String::from(
-                    "https://ci.concourse-ci.org/public/images/favicon-pending.png",
-                ),
-                message: String::from(""),
-            },
-        };
-        if let Some(color) = params.color.as_ref() {
-            message.color = color.clone();
-        }
-        message
-    }
-
-    fn to_message(self, build_metadata: BuildParameters, channel: String) -> slack_push::Message {
-        let (text, build_url) =
-            if let (Some(build_pipeline_name), Some(build_job_name), Some(build_name)) = (
-                build_metadata.build_pipeline_name,
-                build_metadata.build_job_name,
-                build_metadata.build_name,
-            ) {
-                let text = format!("{}/{} #{}", build_pipeline_name, build_job_name, build_name,);
-                let build_url = format!(
-                    "{}/teams/{}/pipelines/{}/jobs/{}/builds/{}",
-                    build_metadata.atc_external_url,
-                    urlencoding::encode(&build_metadata.build_team_name),
-                    urlencoding::encode(&build_pipeline_name),
-                    urlencoding::encode(&build_job_name),
-                    urlencoding::encode(&build_name),
-                );
-                (Some(text), Some(build_url))
-            } else {
-                (Some(String::from("unknown job")), None)
-            };
-        slack_push::Message {
-            channel: channel,
-            attachments: Some(vec![slack_push::message::Attachment {
-                author_name: text,
-                color: Some(self.color),
-                footer: build_url,
-                footer_icon: Some(self.icon_url),
-                ..Default::default()
-            }]),
-            ..Default::default()
+impl Default for OutParams {
+    fn default() -> Self {
+        Self {
+            alert_type: AlertType::default(),
+            color: None,
+            concise: false,
+            message: None,
+            channel: None,
         }
     }
 }
@@ -179,7 +95,7 @@ impl Resource for Test {
             let message = Message::new(&params);
             reqwest::Client::new()
                 .post(reqwest::Url::parse(&source.url).expect("invalid WebHook URL"))
-                .json(&message.to_message(Self::build_metadata(), params.channel))
+                .json(&message.to_slack_message(Self::build_metadata(), params))
                 .send()
                 .unwrap()
                 .text()
@@ -197,3 +113,16 @@ impl Resource for Test {
 }
 
 create_resource!(Test);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_deserialize_params() {
+        let params = r#"{}"#;
+
+        let params = serde_json::from_str::<OutParams>(params);
+        assert!(dbg!(params).is_ok());
+    }
+}
---
+++ src/message.rs
@@ -0,0 +1,138 @@
+use serde::Serialize;
+
+use crate::{AlertType, OutParams};
+use concourse_resource::BuildParameters;
+
+#[derive(Serialize)]
+pub struct Message {
+    pub color: String,
+    pub text: Option<String>,
+    pub icon_url: String,
+}
+
+fn name_and_url_from_params(build_metadata: &BuildParameters) -> (Option<String>, Option<String>) {
+    if let (Some(build_pipeline_name), Some(build_job_name), Some(build_name)) = (
+        build_metadata.build_pipeline_name.as_ref(),
+        build_metadata.build_job_name.as_ref(),
+        build_metadata.build_name.as_ref(),
+    ) {
+        let job_name = format!("{}/{} #{}", build_pipeline_name, build_job_name, build_name,);
+        let build_url = format!(
+            "{}/teams/{}/pipelines/{}/jobs/{}/builds/{}",
+            build_metadata.atc_external_url,
+            urlencoding::encode(&build_metadata.build_team_name),
+            urlencoding::encode(&build_pipeline_name),
+            urlencoding::encode(&build_job_name),
+            urlencoding::encode(&build_name),
+        );
+        (Some(job_name), Some(build_url))
+    } else {
+        (Some(String::from("unknown job")), None)
+    }
+}
+
+impl Message {
+    pub(crate) fn new(params: &OutParams) -> Message {
+        let mut message = match params.alert_type {
+            AlertType::Success => Message {
+                color: String::from("#32cd32"),
+                icon_url: String::from(
+                    "https://ci.concourse-ci.org/public/images/favicon-succeeded.png",
+                ),
+                text: None,
+            },
+            AlertType::Failed => Message {
+                color: String::from("#d00000"),
+                icon_url: String::from(
+                    "https://ci.concourse-ci.org/public/images/favicon-failed.png",
+                ),
+                text: None,
+            },
+            AlertType::Started => Message {
+                color: String::from("#f7cd42"),
+                icon_url: String::from(
+                    "https://ci.concourse-ci.org/public/images/favicon-started.png",
+                ),
+                text: None,
+            },
+            AlertType::Aborted => Message {
+                color: String::from("#8d4b32"),
+                icon_url: String::from(
+                    "https://ci.concourse-ci.org/public/images/favicon-aborted.png",
+                ),
+                text: None,
+            },
+            AlertType::Custom => Message {
+                color: String::from("#35495c"),
+                icon_url: String::from(
+                    "https://ci.concourse-ci.org/public/images/favicon-pending.png",
+                ),
+                text: None,
+            },
+        };
+        if let Some(color) = params.color.as_ref() {
+            message.color = color.clone();
+        }
+        if let Some(text) = params.message.as_ref() {
+            message.text = Some(std::fs::read_to_string(text).unwrap_or_else(|_| text.clone()))
+        }
+        message
+    }
+
+    pub(crate) fn to_slack_message(
+        self,
+        build_metadata: BuildParameters,
+        params: OutParams,
+    ) -> slack_push::Message {
+        let (job_name, build_url) = name_and_url_from_params(&build_metadata);
+        slack_push::Message {
+            attachments: Some(vec![slack_push::message::Attachment {
+                author_name: if params.concise {
+                    job_name
+                } else {
+                    self.text
+                        .clone()
+                        .or_else(|| serde_json::to_string(&params.alert_type).ok())
+                },
+                text: if params.concise { self.text } else { None },
+                color: Some(self.color),
+                footer: build_url,
+                footer_icon: Some(self.icon_url),
+                fields: if params.concise {
+                    None
+                } else {
+                    Some(vec![
+                        slack_push::message::AttachmentField {
+                            title: Some(String::from("Job")),
+                            value: Some(format!(
+                                "{}/{}",
+                                build_metadata
+                                    .build_pipeline_name
+                                    .as_ref()
+                                    .map(String::as_ref)
+                                    .unwrap_or("unknown-pipeline"),
+                                build_metadata
+                                    .build_job_name
+                                    .unwrap_or_else(|| String::from("unknown-job"))
+                            )),
+                            short: Some(true),
+                        },
+                        slack_push::message::AttachmentField {
+                            title: Some(String::from("Build")),
+                            value: Some(
+                                build_metadata
+                                    .build_name
+                                    .unwrap_or_else(|| String::from("unknown-build")),
+                            ),
+                            short: Some(true),
+                        },
+                    ])
+                },
+                ..Default::default()
+            }]),
+            channel: params.channel,
+
+            ..Default::default()
+        }
+    }
+}
